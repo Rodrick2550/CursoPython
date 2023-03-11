4906a19f74f773495d0ad022b6740afb32038e25
rodrick <213480@ids.upchiapas.edu.mx>
2023-03-11 03:44:32 -0600

    improve functionality

--- Ejericio6/Ejercicio2.py
+++ Ejericio6/Ejercicio2.py
@@ -2,14 +2,24 @@ class Student:
     Nombre = None
     Calificacion = None
 
-    def __init__(self, Calificacion) :
-        self.Nombre = 'Rodrigo'
+    def __init__(self,Nombre, Calificacion) :
+        self.Nombre = Nombre
         self.Calificacion = Calificacion
-        if(Calificacion < 6):
-            print('Alumno reprobado')
+    def imprimir(self):
+       print(f"Nombre del alumno: {self.Nombre}")
+       print(f"Nombre del alumno: {self.Calificacion}")
+
+    def evaluar(self):
+        if(self.Calificacion < 6):
+            print('Reprobado')
         else:
-            print('Alumno Aprobado')
-   
+            print('Aprobado')
+        
+
+
+Nombre = input('Ingresa el nombre del alumno \n')
 Nota = int(input('Ingresa la calificacion del estudiante \n'))
-Estudiante = Student(Nota)
+Estudiante = Student(Nombre, Nota)
+Estudiante.imprimir()
+Estudiante.evaluar()
 #print(Estudiante)
